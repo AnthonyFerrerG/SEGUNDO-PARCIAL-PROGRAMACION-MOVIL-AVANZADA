86709b3e66c7cec260fd9036843ae082da14f821
AnthonyFerrerG <anthonydario999710@gmail.com>
2025-12-10 11:15:48 -0300

    Implement RecyclerActivity with RecyclerView setup

---
+++ RecyclerActivity.kt
@@ -0,0 +1,21 @@
+package com.example.materiasapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val listaMaterias = intent.getParcelableArrayListExtra<Materia>("materias") ?: arrayListOf()
+        val adapter = MateriaRecyclerAdapter(listaMaterias)
+        recyclerView.adapter = adapter
+    }
+}
